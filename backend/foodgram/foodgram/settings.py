ff544ab29014942557665c475e5d0dabf4855c71
Alexey Petukhov <69753101+memphis94@users.noreply.github.com>
2023-01-06 14:09:11 +0300

    apps registred

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fsnrknk&*4_m_1n$3af-uogk5q3_c)%ha=w8)7s2r9fa$x$z!w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'django_filters',
+    'api.apps.ApiConfig',
+    'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
 ]
 
 MIDDLEWARE = [
